68229e294f08fa0853cfc7c6b6d16d609bccd6ef
Alexandru Bucur <aleks.bucur27@gmail.com>
2018-06-09 18:25:31 +0300

    add css for contact section

--- resources/views/partials/contact.blade.php
+++ resources/views/partials/contact.blade.php
@@ -1,14 +1,21 @@
-<section class="container" id="contact">
-  <div class="row justify-content-md-center">
+<section class="container contact">
+  <div class="row  pt-5">
+    <div class="col-md-1"></div>
+    <div class="col-md-7">
+      <h3>Get In Touch</h3>
+      <h5 class="mt-0">In order to get in touch use the contact form below:</h5>
+    </div>
+  </div>
+  <div class="row justify-content-md-center pb-5">
     <div class="col-md-7">
       @php dynamic_sidebar('sidebar_contact_fp') @endphp
     </div>
     <div class="col-md-3 d-flex flex-column">
-      <div><b>Quisque Hendrerit</b>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-      <div class="d-flex flex-column">
-        <div><i class="fa fa-map-marker-alt"></i> Elm St. 14/05 Lost City</div>
-        <div><i class="fa fa-phone"></i> +3528 331 86 35</div>
-        <div><i class="fa fa-envelope"></i > info@hexalcorp.com</div>
+      <div class="text-justify"><b>Quisque Hendrerit</b> Donec ac lectus nec dui accumsan pulvinar. Vivamus sit amet lorem nunc. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sit amet felis turpis. Proin quis est eu massa viverra ornare sit amet vel ex. Sed vestibulum finibus velit.</div>
+      <div class="d-flex flex-column mt-5">
+        <div><i class="fa fa-map-marker-alt"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Elm St. 14/05 Lost City</div>
+        <div><i class="fa fa-phone"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;+3528 331 86 35</div>
+        <div><i class="fa fa-envelope"></i >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;info@hexalcorp.com</div>
       </div>
     </div>
   </div>
--- resources/views/template-contact.blade.php
+++ resources/views/template-contact.blade.php
@@ -6,6 +6,5 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('partials.page-header')
     @include('partials.contact')
 @endsection

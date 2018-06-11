837904e8b96953697bb3c919b6070fc6bb95eed6
Alexandru Bucur <aleks.bucur27@gmail.com>
2018-06-11 11:24:38 +0300

    add portfolio html&css

--- resources/views/partials/portfolio.blade.php
+++ resources/views/partials/portfolio.blade.php
@@ -1,6 +1,35 @@
-<section class="d-flex flex-wrap">
-  <img src="@asset('images/portfolio/Layer 31.png')" width="180" height="206" alt="">
-  <img src="@asset('images/portfolio/Layer 33.png')" width="180" height="206" alt="">
+<section class="portfolio">
+  <div id="grid" class="clear">
+    <div class="rw">
+      <div class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap hex-md-dnone"><div class="hexagon"></div></div>
+    </div>
+    <div class="rw">
+      <div class="wrap hex-md-dnone"><div class="hexagon"></div></div>
+      <div class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap hex-md-dnone"><div class="hexagon"></div></div>
+      <div class="wrap hex-md-dnone"><div class="hexagon"></div></div>
+    </div>
+    <div class="rw hex-lg-last">
+      <div class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap" class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap hex-md-dnone"><div class="hexagon"></div></div>
+    </div>
+    <div class="rw">
+      <div class="wrap hex-lg-dnone"><div class="hexagon"></div></div>
+      <div class="wrap hex-lg-dnone"><div class="hexagon"></div></div>
+    </div>
+    <div class="rw">
+      <div class="wrap hex-lg-dnone"><div class="hexagon"></div></div>
+      <div class="wrap hex-lg-dnone" class="wrap"><div class="hexagon"></div></div>
+      <div class="wrap hex-lg-dnone" class="wrap"><div class="hexagon"></div></div>
+    </div>
+  </div>
+<!--  <img src="@asset('images/portfolio/Layer 33.png')" width="180" height="206" alt="">
   <img src="@asset('images/portfolio/Layer 34.png')" width="180" height="206" alt="">
   <img src="@asset('images/portfolio/Layer 35.png')" width="180" height="206" alt="">
   <img src="@asset('images/portfolio/Layer 36.png')" width="180" height="206" alt="">
@@ -11,5 +40,5 @@
   <img src="@asset('images/portfolio/Layer 41.png')" width="180" height="206" alt="">
   <img src="@asset('images/portfolio/Layer 42.png')" width="180" height="206" alt="">
   <img src="@asset('images/portfolio/Layer 43.png')" width="180" height="206" alt="">
-  <img src="@asset('images/portfolio/Layer 44.png')" width="180" height="206" alt="">
+  <img src="@asset('images/portfolio/Layer 44.png')" width="180" height="206" alt=""> -->
 </section>
